dc0da29e53b88aebb7c695d15034f9ebb9b51368
zhangyongliang <jrrexliang@live.com>
2019-07-04 11:36:22 +0800

    添加Druid配置文件

---
+++ src/main/java/com/getsource/base/config/DruidConfig.java
@@ -0,0 +1,52 @@
+package com.getsource.base.config;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhangyongliang
+ * @date 2018-11-19
+ */
+@Configuration
+public class DruidConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(DruidConfig.class);
+
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        log.info("init Druid Servlet Configuration ");
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean();
+        servletRegistrationBean.setServlet(new StatViewServlet());
+        servletRegistrationBean.addUrlMappings("/druid/*");
+        Map<String, String> initParameters = new HashMap<String, String>(5);
+        // 用户名
+        initParameters.put("loginUsername", "admin");
+        // 密码
+        initParameters.put("loginPassword", "123456");
+        // 禁用HTML页面上的“Reset All”功能
+        initParameters.put("resetEnable", "false");
+        // IP白名单 (没有配置或者为空，则允许所有访问)
+        initParameters.put("allow", "");
+        servletRegistrationBean.setInitParameters(initParameters);
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+
+}
